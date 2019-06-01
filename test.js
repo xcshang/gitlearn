7d572c951a2cc67de5e719d5185b0fc4ef7d90b9
xcshang <lalasxc@126.com>
2019-06-01 22:38:19 +0800

    ddd

--- test.js
+++ test.js
@@ -5,25 +5,72 @@ var webdriver = require('selenium-webdriver'),
     .forBrowser('chrome')
     .build();
 
-driver.get('https://dl.reg.163.com/ydzj/mob?product=urs&curl=https://reg.163.com#/pwdLogin');
-driver.sleep(5000);//毫秒
+let options = driver.manage() 
+driver.get('https://m.reg.163.com/?email=1/#/email');
+
+setTimeout(() => {
+	driver.findElement(By.linkText('立即登录')).click()
+	driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+	fillUserName()
+	
+},2000)
+
+
+function fillPassword(){
+	setTimeout(() => {
+	driver.findElement(By.name('password')).sendKeys('wjsssrdbb1212!5')
+	loginBtn()
+},1000)
+	
+	
+}
+
+
+function fillUserName(){
+	setTimeout(() => {
+	driver.findElement(By.css('input[placeholder=邮箱帐号或手机号]')).sendKeys('lalasxc@126.com')
+	fillPassword()
+	
+},1000)
+}
+
+function loginBtn(){
+	setTimeout(() => {
+	driver.findElement(By.id('dologin')).click()
+	getCookie()
+	
+},1000)
+}
+
+function getCookie(){
+	setTimeout(() => {
+	options.getCookie('P_INFO').then((ck)=>{
+		if(ck.value){
+			process.stdout.write('success');
+			driver.quit();
+		}
+	})
+	
+},2000)
+}
+
+
+
+
+/*
 driver.executeScript('return document.cookie').then(function(obj){
     process.stdout.write('success');
-    //process.exit(0)
     driver.quit();
 }).catch((e) => {
-	process.stdout.write(e);
-	//process.exit(0)
+	process.stdout.write('fail');
 	driver.quit();
 })
-//driver.sleep(10000);//毫秒
-//process.stdout.write('success');
-//driver.quit();
+*/
 
 //process.stdout.write('success');
 //driver.findElement(By.className('ipt_input ipt_input_large ipt_input-')).sendKeys('18668423158');
 //driver.findElement(By.className('ipt_input ipt_input_large ipt_input-')).sendKeys('18668423158');
-//driver.findElement(By.className('f-right')).click();
-//driver.wait(dd, 1000);
+//driver.findElement(By.xpath('<a class="f-right">密码登录</a>')).click();
+//driver.wait(driver.findElement(By.linkText('密码登录')).click(), 1000);
 //driver.quit();
  
