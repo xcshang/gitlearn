2d7afc0d4d2c7ba090a95edcdcbb98ffeaee3108
xcshang <lalasxc@126.com>
2019-06-01 20:21:03 +0800

    ddd

--- test.js
+++ test.js
@@ -6,19 +6,21 @@ var webdriver = require('selenium-webdriver'),
     .build();
 
 driver.get('https://dl.reg.163.com/ydzj/mob?product=urs&curl=https://reg.163.com#/pwdLogin');
-driver.sleep(5000);//毫秒
+//driver.sleep(5000);//毫秒
 driver.executeScript('return document.cookie').then(function(obj){
-    console.log(obj)
     process.stdout.write('success');
+    //process.exit(0)
     driver.quit();
 }).catch((e) => {
 	process.stdout.write(e);
+	//process.exit(0)
+	driver.quit();
 })
 //driver.sleep(10000);//毫秒
 //process.stdout.write('success');
 //driver.quit();
 
-
+//process.stdout.write('success');
 //driver.findElement(By.className('ipt_input ipt_input_large ipt_input-')).sendKeys('18668423158');
 //driver.findElement(By.className('ipt_input ipt_input_large ipt_input-')).sendKeys('18668423158');
 //driver.findElement(By.className('f-right')).click();
